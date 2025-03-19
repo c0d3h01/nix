1834f94c4894ac34d8cb94f445a1486758d4a310
c0d3h01 <c0d3h01@gmail.com>
2025-03-19 12:15:19 +0530

    rm printing.nix extras

--- nix/apps/printing.nix
+++ nix/apps/printing.nix
@@ -4,7 +4,6 @@
   # -*- Enable printing -*-
   services.printing.enable = true;
   services.avahi.enable = true;
-  services.avahi.nssmdns4 = true;
   services.avahi.openFirewall = true;
-  environment.systemPackages = [ pkgs.cups-filters ];
+  networking.firewall.allowedTCPPorts = [ 631 ];
 }
