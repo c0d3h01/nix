b6187f462e2218da0e8c4a3df049e8cd44e0c8c7
c0d3h01 <c0d3h01@gmail.com>
2025-03-26 14:43:30 +0530

    upd: remove wifi password and related secrets from configuration

--- secrets.nix
+++ secrets.nix
@@ -5,9 +5,5 @@
   imports = [ agenix.nixosModules.default ];
   environment.systemPackages = [ agenix.packages.${system}.default ];
   age.secrets = {
-    wifi-password = {
-      file = ./secrets/wifi-password.age;
-      owner = "${username}";
-    };
   };
 }
