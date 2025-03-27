9594de66ca171393c57e561365c2f09cb5cfb7fa
c0d3h01 <c0d3h01@gmail.com>
2025-03-27 09:32:05 +0530

    upd: enhance android.nix configuration with additional environment variables and path exports

--- nix/apps/devtools/android.nix
+++ nix/apps/devtools/android.nix
@@ -19,15 +19,17 @@ let
 in
 {
   users.users.${username}.extraGroups = [ "adbusers" ];
-  environment.variables = {
-    ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
-    ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
-    ANDROID_NDK_ROOT = "${androidSdk}/libexec/android-sdk/ndk/22.0.7026061";
+  environment = {
+    variables = {
+      ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+      ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+      ANDROID_NDK_ROOT = "${androidSdk}/libexec/android-sdk/ndk/22.0.7026061";
+    };
+    extraInit = ''
+      export PATH="${androidSdk}/libexec/android-sdk/ndk/22.0.7026061:$PATH"
+      export PATH="${androidSdk}/libexec/android-sdk/platform-tools:$PATH"
+      export PATH="${androidSdk}/libexec/android-sdk/cmdline-tools/latest/bin:$PATH"
+      export PATH="${androidSdk}/libexec/android-sdk/build-tools/30.0.3:$PATH"
+    '';
   };
-  environment.pathsToLink = [
-    "/libexec/android-sdk/ndk/22.0.7026061"
-    "/libexec/android-sdk/platform-tools"
-    "/libexec/android-sdk/cmdline-tools/latest/bin"
-    "/libexec/android-sdk/build-tools/30.0.3"
-  ];
 }
