586acbccd411ee513e72c1c610567c4230c52eef
c0d3h01 <c0d3h01@gmail.com>
2025-04-03 15:12:21 +0530

    refactor: standardize formatting in firefox default.nix configuration

--- home/modules/firefox/default.nix
+++ home/modules/firefox/default.nix
@@ -18,7 +18,7 @@ in
 
       profiles.${user.username} = {
         # Essential extensions only
-        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           privacy-badger
           link-cleaner
@@ -64,7 +64,7 @@ in
 
         # UserChrome CSS for consistent theming
         userChrome = "
-          * { 
+          * {
               box-shadow: none !important;
               border: 0px solid !important;
           }
@@ -77,7 +77,7 @@ in
               border-radius: var(--user-tab-rounding) var(--user-tab-rounding) 0px 0px !important;
               margin-block: 1px 0 !important;
           }
-                    
+
           #scrollbutton-up, #scrollbutton-down {
               border-top-width: 1px !important;
               border-bottom-width: 0 !important;
@@ -87,7 +87,7 @@ in
               --lwt-tabs-border-color: rgba(0, 0, 0, 0.5) !important;
               border-bottom-color: transparent !important;
           }
-                    
+
           [brighttext='true'] .tab-background:is([selected], [multiselected]):-moz-lwtheme {
               --lwt-tabs-border-color: rgba(255, 255, 255, 0.5) !important;
               border-bottom-color: transparent !important;
@@ -101,12 +101,12 @@ in
           #TabsToolbar, #tabbrowser-tabs {
               --tab-min-height: 29px !important;
           }
-                    
-          #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar, 
+
+          #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar,
           #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar #tabbrowser-tabs {
               --tab-min-height: 30px !important;
           }
-                    
+
           #scrollbutton-up,
           #scrollbutton-down {
               border-top-width: 0 !important;
@@ -116,9 +116,9 @@ in
           #TabsToolbar, #TabsToolbar > hbox, #TabsToolbar-customization-target, #tabbrowser-arrowscrollbox  {
               max-height: calc(var(--tab-min-height) + 1px) !important;
           }
-                    
-          #TabsToolbar-customization-target toolbarbutton > .toolbarbutton-icon, 
-          #TabsToolbar-customization-target .toolbarbutton-text, 
+
+          #TabsToolbar-customization-target toolbarbutton > .toolbarbutton-icon,
+          #TabsToolbar-customization-target .toolbarbutton-text,
           #TabsToolbar-customization-target .toolbarbutton-badge-stack,
           #scrollbutton-up,#scrollbutton-down {
               padding-top: 7px !important;
