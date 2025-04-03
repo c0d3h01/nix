1da03fa5fb86a2760ab9c3600fe67392d5c60c72
c0d3h01 <c0d3h01@gmail.com>
2025-04-03 16:55:16 +0530

    refactor: update flake.nix for improved configuration and dependency management

--- home/flake.nix
+++ home/flake.nix
@@ -1,36 +1,45 @@
 {
-  description = "Home Manager configuration of c0d3h01";
+  description = "Standalone Home Manager configuration of c0d3h01";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, nur, ... } @ inputs:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [ nur.overlays.default ];
+      };
 
-      username = "c0d3h01";
-      hostname = "NixOS";
-      useremail = "c0d3h01@gmail.com";
-
-      specialArgs = {
-        inherit username hostname useremail;
+      user = {
+        fullName = "Harshal Sawant";
+        username = "c0d3h01";
+        stateVersion = "24.11";
+        hostname = "NixOS";
+        email = "c0d3h01@gmail.com";
       };
     in
     {
-      homeConfigurations."${username}" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations.${user.username} = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        extraSpecialArgs = specialArgs;
+        extraSpecialArgs = { inherit user inputs; };
         modules = [
           ./default.nix
-          ({ config, ... }: {
-            nixpkgs.config.allowUnfree = true;
-          })
         ];
       };
     };
