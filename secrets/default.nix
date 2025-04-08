c9d7574872c5405e5c05f0e6db12c98ebf59f60e
c0d3h01 <c0d3h01@gmail.com>
2025-04-08 15:41:45 +0530

    upd: refactor age secrets configuration in default.nix

--- secrets/default.nix
+++ secrets/default.nix
@@ -7,8 +7,5 @@
 
   imports = [ agenix.nixosModules.default ];
   environment.systemPackages = [ agenix.packages.x86_64-linux.default pkgs.age ];
-
-  age = {
-    secrets = import ./secrets.nix;
-  };
+  age.secrets = import ./secrets.nix;
 }
