4c5452105cd714d95d5ef2c5552b86a4dbfcb2e5
c0d3h01 <c0d3h01@gmail.com>
2025-04-08 15:43:06 +0530

    upd: update SSH private key ownership and group in secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,13 +3,11 @@ let
 in
 with keys;
 {
-  # SSH Key
   "ssh-private-key" = {
     file = ./ssh-private-key.age;
-    owner = "c0d3h01";
-    group = "users";
+    owner = "root";
+    group = "root";
     mode = "600";
-    path = "/home/c0d3h01/.ssh/id_ed25519";
-    publicKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMHEDTYxp5o3ZLpEg4fWjXZ/MctVco8R9qYVfE0tIn+ c0d3h01@gmail.com" ];
+    publicKey = [ keys.c0d3h01 ];
   };
 }
