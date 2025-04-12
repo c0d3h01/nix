37920c5a795d8a7af1236815963fc7626779d383
Harshal Sawant <haarshalsawant@gmail.com>
2025-04-12 06:54:09 +0530

    style: restructured secrets config

--- keys.nix
+++
@@ -1,3 +0,0 @@
-{
-  c0d3h01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYgWFz+XI6zQvE23I9ZOvewv8ybkbjgVwoA/8rmocsE haarshalsawant@gmail.com";
-}
--- secrets/default.nix
+++ secrets/default.nix
@@ -1,11 +1,6 @@
-{ config
-, pkgs
-, inputs
-, ...
-}: {
+{ config, pkgs, inputs, ... }: {
+
   imports = [ inputs.agenix.nixosModules.default ];
   environment.systemPackages = [ inputs.agenix.packages.x86_64-linux.default pkgs.age ];
-  age = {
-    secrets = import ./secrets.nix;
-  };
+  age = { secrets = import ./secrets.nix; };
 }
