e67325b4a377d5e6a4fce59bcbdcc05141b3dc66
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-04 02:21:55 +0530

    feat(fonts): add fontconfig settings and update font packages

--- nixosModules/fonts.nix
+++ nixosModules/fonts.nix
@@ -10,31 +10,27 @@
     enableDefaultPackages = false;
 
     packages = with pkgs; [
+      dejavu_fonts
       hack-font
       twitter-color-emoji
       noto-fonts
       noto-fonts-cjk-sans
+      noto-fonts-extra
     ];
 
     fontconfig = {
       enable = true;
+      antialias = true;
+      hinting.enable = true;
+      hinting.style = "slight"; # Options: "none", "slight", "medium", "full"
+      subpixel.lcdfilter = "default";
+      subpixel.rgba = "rgb";    # Options: "rgb", "bgr", "vrgb", "vbgr", "none"
 
       defaultFonts = {
-        serif = lib.mkForce [
-          "Noto Serif"
-        ];
-
-        sansSerif = lib.mkForce [
-          "Noto Sans"
-        ];
-
-        monospace = lib.mkForce [
-          "Hack"
-        ];
-
-        emoji = lib.mkForce [
-          "Twitter Color Emoji"
-        ];
+        serif = lib.mkForce [ "DejaVu Serif" "Noto Serif" ];
+        sansSerif = lib.mkForce [ "DejaVu Sans" "Noto Sans" ];
+        monospace = lib.mkForce [ "Hack" ];
+        emoji = lib.mkForce [ "Twitter Color Emoji" ];
       };
     };
   };
