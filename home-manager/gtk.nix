9d96a874d56da02dc4d0e8d0fa8d6c12f81536c5
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-08 11:58:23 +0530

    feat(gtk): update GTK theme and cursor settings to use Adwaita and improve configuration

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -5,24 +5,35 @@
     enable = true;
 
     theme = lib.mkDefault {
-      name = "WhiteSur-Dark";
-      package = pkgs.whitesur-gtk-theme;
-    };
-
-    cursorTheme = lib.mkDefault {
-      name = "Bibata-Modern-Ice";
-      package = pkgs.bibata-cursors;
+      name = "Adwaita";
+      package = pkgs.gnome-themes-extra;
     };
 
     iconTheme = lib.mkDefault {
-      name = "WhiteSur-dark";
-      package = pkgs.whitesur-icon-theme;
+      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    cursorTheme = lib.mkDefault {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+
+    # GTK optimization settings
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+      gtk-enable-animations = 1;
+      gtk-toolbar-style = "GTK_TOOLBAR_BOTH_HORIZ";
+      gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
     };
   };
 
-  # qt = {
-  #   enable = true;
-  #   platformTheme.name = "gtk";
-  #   style.name = "adwaita-dark";
-  # };
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+    style.name = "adwaita-dark";
+  };
 }
