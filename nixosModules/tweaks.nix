609e241e3ed354765ef3590c3344b3d16c6f781c
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-21 20:42:29 +0530

    chore(tlp): tlp commented, enabled cpu governor to shed...

--- nixosModules/tweaks.nix
+++ nixosModules/tweaks.nix
@@ -15,18 +15,18 @@
     package = pkgs.ananicy-cpp;
   };
 
-  services.tlp = {
-    enable = true;
-    settings = {
-      USB_AUTOSUSPEND = 1;
-      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    };
-  };
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     # USB_AUTOSUSPEND = 1;
+  #     CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #   };
+  # };
 
   powerManagement = {
     enable = true;
     powertop.enable = true;
-    # cpuFreqGovernor = "schedutil";
+    cpuFreqGovernor = "schedutil";
   };
 }
