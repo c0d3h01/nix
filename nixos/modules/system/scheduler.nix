abaf0911ff36b5b00e0394d8b0d6632433dcb186
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-24 16:54:21 +0530

    fix: enable power-profiles-daemon service in scheduler configuration

--- nixos/modules/system/scheduler.nix
+++ nixos/modules/system/scheduler.nix
@@ -4,11 +4,12 @@
   lib,
   ...
 }:
-
 {
   services.scx = lib.mkIf userConfig.machineConfig.workstation.enable {
     enable = true;
     scheduler = "scx_flash";
     package = pkgs.scx.rustscheds;
   };
+
+  services.power-profiles-daemon.enable = false;
 }
