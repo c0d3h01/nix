1578b160cbbf3bddee9eee497e2f56bb2b3569d3
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-24 16:54:02 +0530

    fix: remove XDG_DATA_DIRS session variable from flatpak configuration

--- nixos/modules/system/flatpak.nix
+++ nixos/modules/system/flatpak.nix
@@ -7,6 +7,5 @@
   # flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
   config = lib.mkIf userConfig.machineConfig.workstation.enable {
     services.flatpak.enable = false;
-    environment.sessionVariables.XDG_DATA_DIRS = [ "/var/lib/flatpak/exports/share" ];
   };
 }
