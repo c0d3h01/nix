7a89e377bf9c1744695ced506b9fa337c5012c95
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-09-03 11:39:28 +0530

    refactor: restructure syncthing module configuration

--- nixos/modules/services/syncthing.nix
+++ nixos/modules/services/syncthing.nix
@@ -1,8 +1,16 @@
 {
-  # ssh -L 9999:localhost:8384 nixos
-  services.syncthing = {
-    enable = true;
-    openDefaultPorts = true;
-    dataDir = "/srv/storage/syncthing";
+  lib,
+  config,
+  userConfig,
+  ...
+}:
+{
+  config = lib.mkIf (userConfig.machineConfig.type == "server") {
+    # ssh -L 9999:localhost:8384 nixos
+    services.syncthing = {
+      enable = true;
+      openDefaultPorts = true;
+      dataDir = "/srv/storage/syncthing";
+    };
   };
 }
