e2575f11166a7eccc752a485b91a676ea77c272c
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-10-24 15:00:58 +0000

    cleanup: bat extrapackages

--- home-manager/modules/terminal/bat.nix
+++ home-manager/modules/terminal/bat.nix
@@ -1,15 +1,8 @@
-{ pkgs, ... }:
 {
   programs.bat = {
     enable = true;
     config = {
       theme = "TwoDark";
     };
-    extraPackages = with pkgs.bat-extras; [
-      batdiff
-      batman
-      batgrep
-      batwatch
-    ];
   };
 }
